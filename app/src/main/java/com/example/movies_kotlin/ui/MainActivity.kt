fd894d220430de3b2e2064e584e90519300e3413
aazyabin <azyabon9@gmail.com>
2023-04-15 15:19:35 +0700

    feat: pagination class

--- app/src/main/java/com/example/movies_kotlin/ui/MainActivity.kt
+++ app/src/main/java/com/example/movies_kotlin/ui/MainActivity.kt
@@ -11,12 +11,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val btn = findViewById<Button>(R.id.btn)
-
-        btn.setOnClickListener {
-            val intent = Intent(this, SingleMovie::class.java)
-            intent.putExtra("id", 299534)
-            this.startActivity(intent)
-        }
     }
 }
